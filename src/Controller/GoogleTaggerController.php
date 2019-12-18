d3086d244d41e5789533cea032b357c93710fc0f
Roma K <romankuchukov@gmail.com>
2019-12-18 18:39:54 -0500

    added DI

--- src/Controller/GoogleTaggerController.php
+++ src/Controller/GoogleTaggerController.php
@@ -3,11 +3,37 @@
 namespace Drupal\google_tagger\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a Google TAG form variable.
  */
 class GoogleTaggerController extends ControllerBase {
+
+  /**
+   * Declare config field.
+   *
+   * @var Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $config;
+
+  /**
+   * Constructs a new Config object.
+   *
+   * @param Drupal\Core\Config\ConfigFactoryInterface $config
+   *   Creates config for the module.
+   */
+  public function __construct(ConfigFactoryInterface $config) {
+    $this->config = $config->get('google_tagger.settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('config.factory'));
+  }
 
   /**
    * {@inheritdoc}
@@ -16,7 +42,7 @@ class GoogleTaggerController extends ControllerBase {
 
     return [
       '#theme' => 'google-tagger-id',
-      '#google_tagger_id' => \Drupal::config('google_tagger.settings')->get('tagger-id'),
+      '#google_tagger_id' => $this->config->get('tagger-id'),
     ];
   }
 
